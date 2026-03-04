81b63770878ed021abb221da6ab468a01c3e5e70
Simon <simonazike155@gmail.com>
2026-03-04 15:10:45 +0000

    fix: remove dotenv, use Render env vars directly

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const { startScheduler } = require('./services/scheduler')
 
 
 
 
-dotenv.config()
+
 connectDB()
 startScheduler()
 
