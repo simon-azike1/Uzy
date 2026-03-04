329f90503129b2ef6b6fdc6073429338d49848ad
Simon <simonazike155@gmail.com>
2026-03-04 14:41:13 +0000

    fix: use VITE_API_URL in axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 })
 
 API.interceptors.request.use((config) => {
@@ -10,6 +10,17 @@ API.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${user.token}`
   }
   return config
+
+
 })
+
+
+export const emailAPI = {
+  getStatus:             () => API.get('/email/status'),
+  connectGoogle:         () => API.get('/email/google/connect'),
+  connectMicrosoft:      () => API.get('/email/microsoft/connect'),
+  disconnectGoogle:      () => API.delete('/email/google/disconnect'),
+  disconnectMicrosoft:   () => API.delete('/email/microsoft/disconnect'),
+}
 
 export default API
